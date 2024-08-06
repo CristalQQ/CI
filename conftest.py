a887c0e2cf9586916f8b4acb68904b93b4982638
Vladislav Kobzar <131853000+vvkobzar@users.noreply.github.com>
2024-08-06 21:53:43 +0300

    removed service

--- conftest.py
+++ conftest.py
@@ -1,12 +1,10 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-service = Service(ChromeDriverManager().install())
+
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-blink-features=AutomationControlled")
@@ -16,14 +14,14 @@ options.add_argument(
 
 @pytest.fixture()
 def driver():
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
 
 @pytest.fixture()
 def page(request):
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(options=options)
     param = request.param
     if param == "whats_new":
         driver.get("https://magento.softwaretestingboard.com/what-is-new.html")
